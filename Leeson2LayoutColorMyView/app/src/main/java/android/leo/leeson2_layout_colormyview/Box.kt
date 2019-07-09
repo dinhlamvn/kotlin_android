fdd9bb1fe46c79c4b61ed4e29f355781695df722
dinhlamvn <dinhlamvn353@gmail.com>
2019-07-09 10:07:33 +0700

    apply data binding to color myview app in lesson 2

---
+++ Leeson2LayoutColorMyView/app/src/main/java/android/leo/leeson2_layout_colormyview/Box.kt
@@ -0,0 +1,9 @@
+package android.leo.leeson2_layout_colormyview
+
+data class Box(
+        var color1 : Int = android.R.color.white,
+        var color2 : Int = android.R.color.white,
+        var color3 : Int = android.R.color.white,
+        var color4 : Int = android.R.color.white,
+        var color5 : Int = android.R.color.white
+)
